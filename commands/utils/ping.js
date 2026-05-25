4c50b7ee6bf4c2336713e3c6e5b60bb9ade6c69d
Ryzen <125817626+Ryzen-Developper@users.noreply.github.com>
2026-05-24 23:52:39 -0300

    Delete commands directory

--- commands/utils/ping.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = {
-    data: new SlashCommandBuilder().setName('ping').setDescription('Latência do bot'),
-    async execute(interaction) {
-        await interaction.reply(`🏓 Pong! Latência: ${interaction.client.ws.ping}ms`);
-    }
-};
